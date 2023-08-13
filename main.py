1d321a8c5daf3dc35ac75cdef6012744fd1a7c16
Teerat-CH <81249482+Teerat-CH@users.noreply.github.com>
2023-08-13 14:24:02 +0700

    update site description

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ option = st.sidebar.radio("Select Page", ["Home", "Interactive Plots", "Package 
 
 if option == "Home":
     st.title("StarClusterPlot")
-    st.write("[StarClusterPlot](https://pypi.org/project/StarClusterPlot/0.1.7/) is a Python package created and maintained by [Teerat Chanromyen](https://cosmicdusts.wordpress.com/about/) to help visualize star cluster structure and the Hertzsprung-Russell Diagram with real, calculated color, specifically designed to work with data from the [Sloan Digital Sky Survey](https://www.sdss.org/).")
-    st.write("This site served as a documentation hub, featuring examples of interactive plots, a package tutorial, and technical documentation. To explore each part, simply select a topic from the left-hand sidebar.")
+    st.write("[StarClusterPlot](https://pypi.org/project/StarClusterPlot/0.1.7/) is a Python package created and maintained by [Teerat Chanromyen](https://cosmicdusts.wordpress.com/about/) that helps visualize a star cluster's structure and its Hertzsprung-Russell Diagram with real, calculated color. It is specifically designed to work with data from the [Sloan Digital Sky Survey](https://www.sdss.org/) or any dataset that contains g, r, right ascension, and declination values.")
+    st.write("This site served as a documentation hub, featuring examples of interactive plots, a package tutorial, and technical documentation. To explore each part, select a topic from the left-hand sidebar.")
     st.write(" ")
     st.image('M13_HR.png', caption='Hertzsprung-Russell Diagram of Messier 13')
 
